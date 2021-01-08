65bd28013d41c1c880dc7dcc0b78be799b56ab7e
Bruno Gonçalves <bugotech@gmail.com>
2021-01-08 15:30:28 -0300

    @build

--- bin/teste.sh
+++ bin/teste.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 git remote -v
+git push origin HEAD:main
 git filter-branch --subdirectory-filter css/ -f
 git remote set-url origin https://github.com/rhino-org/rhino.css.git
 git fetch origin
